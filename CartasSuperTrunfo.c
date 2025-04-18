a290bc719e940e6d3ce86298f63eae070e3eace9
Rodrigo Docinel <docinel@gmail.com>
2025-04-18 17:51:35 +0000

    Implementado o nome do estado. Densidade Populacional e PIB per Capita.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,8 +8,11 @@ int main()
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     char estado[2]; // {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', '\0'};
     char CodigoCarta[4]; // {'A01', 'B02', 'C03', 'D04', 'E05', 'F06', 'G07', 'H08', '\0'};
+    char NomeCidade[15]; // {'SaoPaulo', 'RiodeJaneiro', 'Brasilia', 'Fortaleza', 'Salvador',
+                        //'BeloHorizonte', 'Manaus', 'Curitiba', 'Recife', 'Goiânia', '\0'}
     int populacao, PontoTuristico;
-    float area, PIB;
+    float area, PIB, DensidadePopulacional, PibPerCapita;
+
     printf("############## DESAFIO SUPER TRUNFO ##############\n\n");
     printf("############## CADASTRO DE CARTAS ################\n");
 
@@ -22,6 +25,10 @@ int main()
     printf("\nDigite o código do Carta: ");
     scanf("%3s", CodigoCarta);
 
+    printf("\nAs cidades são: SaoPaulo, RiodeJaneiro, Brasilia, Fortaleza, \nSalvador, BeloHorizonte, Manaus, Curitiba, Recife, Goiania\n");
+    printf("\nDigite o nome da cidade: ");
+    scanf("%14s", NomeCidade);
+
     printf("Digite a população do Estado: ");
     scanf("%d", &populacao);
 
@@ -34,39 +41,63 @@ int main()
     printf("Há quantos pontos turísticos no Estado: ");
     scanf("%d", &PontoTuristico);
 
+    // Calcular a Densidade Populacional
+    DensidadePopulacional = populacao / area; // hab/km²
+    PibPerCapita = PIB / populacao; // reais
+
     //Saída dos Dados da Carta A
     printf("\n==================================================\n");
     printf("\nEstado: %s", estado);
     printf("\nCódigo: %s", CodigoCarta);
+    printf("\nCidade: %s", NomeCidade);
     printf("\nPopulação: %d", populacao);
     printf("\nArea: %.2f km²", area);
     printf("\nNúmero de pontos turísticos: %d", PontoTuristico);
-    printf("\nPIB: %.2f", PIB);
+    printf("\nPIB: %.2f bilhões de reais", PIB);
+    printf("\nDensidade Populacional: %.2f hab/km²", DensidadePopulacional);
+    printf("\nPIB per Capita: %.2f reais", PibPerCapita);
     printf("\n==================================================\n");
 
     //Entrada de Dados da Carta B
     printf("\nOs nomes dos estados devem estar entre as letras 'A até H'\n");
     printf("\nDigite o nome do Estado: ");
     scanf("%1s", estado);
+
     printf("\nOs códigos dos estados devem estar conforme o exemplo: \n 'A01, B02, C03... H08'\n");
     printf("\nDigite o código do Carta: ");
     scanf("%3s", CodigoCarta);
+
+    printf("\nAs cidades são: SaoPaulo, RiodeJaneiro, Brasilia, Fortaleza, \nSalvador, BeloHorizonte, Manaus, Curitiba, Recife, Goiania\n");
+    printf("\nDigite o nome da cidade: ");
+    scanf("%14s", NomeCidade);
+
     printf("Digite a população do Estado: ");
     scanf("%d", &populacao);
+
     printf("Digite a area do Estado: ");
     scanf("%f", &area);
+
     printf("Digite o PIB do Estado: ");
     scanf("%f", &PIB);
+
     printf("Há quantos pontos turísticos no Estado: ");
     scanf("%d", &PontoTuristico);
+
+    // Calcular a Densidade Populacional
+    DensidadePopulacional = populacao / area; // hab/km²
+    PibPerCapita = PIB / populacao; // reais
+
     //Saída dos Dados da Carta B
     printf("\n==================================================\n");
     printf("\nEstado: %s", estado);
     printf("\nCódigo: %s", CodigoCarta);
+    printf("\nCidade: %s", NomeCidade);
     printf("\nPopulação: %d", populacao);
     printf("\nArea: %.2f km²", area);
     printf("\nNúmero de pontos turísticos: %d", PontoTuristico);
-    printf("\nPIB: %.2f", PIB);
+    printf("\nPIB: %.2f bilhões de reais", PIB);
+    printf("\nDensidade Populacional: %.2f hab/km²", DensidadePopulacional);
+    printf("\nPIB per Capita: %.2f reais", PibPerCapita);
     printf("\n==================================================\n");
     return 0;
 }
